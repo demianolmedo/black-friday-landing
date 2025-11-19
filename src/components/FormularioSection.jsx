dc09eeb7a4131847865fed4d35ce613755e5e14a
DEMIAN OLMEDO CADIMA <demianccs@gmail.com>
2025-11-18 20:29:10 -0400

    button secion margin

--- src/components/FormularioSection.jsx
+++ src/components/FormularioSection.jsx
@@ -193,7 +193,7 @@ const FormularioSection = () => {
     <>
       <section
         id="contact-form"
-        className="relative w-full flex items-center justify-center overflow-hidden py-16 sm:py-24"
+        className="relative w-full flex items-center justify-center overflow-hidden py-8 sm:py-12"
       >
         {/* Background */}
         <div className="absolute inset-0 bg-gradient-to-b from-azul-principal via-azul-principal to-azul-principal"></div>
